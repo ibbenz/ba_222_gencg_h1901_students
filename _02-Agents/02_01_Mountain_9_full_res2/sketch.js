ef28c8dbc3478dc883ee749cd0c37b9239f16d82
Pascal Benz <p.benz@gmx.ch>
2019-12-12 20:58:04 +0100

    update res2

--- _02-Agents/02_01_Mountain_9_full_res2/sketch.js
+++ _02-Agents/02_01_Mountain_9_full_res2/sketch.js
@@ -70,7 +70,7 @@ function setup() {
   mountains= new Array(options.numberOfMountains);
     for (let j = 0; j <= options.numberOfMountains; j++) {
         //(peaks,height,width,iterationDepth){
-        mountains[j]=new mountainElement(options.numberOfPeaks,(windowHeight1 / options.numberOfMountains) * j,1.5*windowWidth1,1);
+        mountains[j]=new mountainElement(options.numberOfPeaks,(2*windowHeight1 / options.numberOfMountains) * j,1.5*windowWidth1,1);
         temporaryWave[j]=0;
         temporaryWaveCounter[j]=0;
     }
@@ -111,7 +111,7 @@ function draw() {
             //Wir machen die Mountains deutlich breiter als die Canvas, so können wir die Peaks innerhalb der
             //Canvas leicht verschieben, ohne auf die Grösse des Arrays zu achten.
             //constructor(peaks,height,width,iterationDepth)
-            mountains[j] = new mountainElement(options.numberOfPeaks, (windowHeight1 / (options.numberOfMountains+1)) * j, 1.5*windowWidth1, 1);
+            mountains[j] = new mountainElement(options.numberOfPeaks, (2*windowHeight1 / (options.numberOfMountains+1)) * j, 1.5*windowWidth1, 1);
             temporaryWave[j] = 0;
             temporaryWaveCounter[j] = 0;
         }
